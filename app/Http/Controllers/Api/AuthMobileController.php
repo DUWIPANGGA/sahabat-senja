fc49f63385172fb68a12816dd84d6e8fc259643f
DUWIPANGGA <aryad2232@gmail.com>
2025-12-09 18:38:20 +0700

    this update login with google

--- app/Http/Controllers/Api/AuthMobileController.php
+++ app/Http/Controllers/Api/AuthMobileController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
-use Illuminate\Support\Str;
 
 class AuthMobileController extends Controller
 {
